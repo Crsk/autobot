3b9ffc2983219c17bddb1a6d0777dfc76e9364ab
Christopher Kiessling <Crsk@mail.com>
2023-03-22 01:59:39 -0300

    🔨 Iterate through all nodes to display them

--- src/automation-engine/components/index.tsx
+++ src/automation-engine/components/index.tsx
@@ -3,6 +3,7 @@ import Grid from '@/automation-engine/components/Grid'
 import Box from '@/automation-engine/components/Box'
 import { addNode } from '@/redux/store'
 import { useDispatch, useSelector } from 'react-redux'
+import { Node } from '@/automation-engine/models/node'
 
 function AutomationEngine() {
   const dispatch = useDispatch()
@@ -10,12 +11,15 @@ function AutomationEngine() {
 
   useEffect(() => {
     dispatch(addNode({ parentId: null, id: '0', x: 300, y: 300 }))
+    dispatch(addNode({ id: '1', x: 400, y: 600, parentId: '0' }))
   }, [dispatch])
 
   return (
     <svg width="100vw" height="100vh">
       <Grid />
-      {node && <Box node={node} />}
+      {(nodes as Node[]).filter((x) => !!x).map((nodee: Node) => (
+        <Box key={nodee.id} node={nodee} />
+      ))}
     </svg>
   )
 }

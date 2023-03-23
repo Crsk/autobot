14eae2ee87551319498aa48160001424befa84d4
Christopher Kiessling <Crsk@mail.com>
2023-03-23 20:26:42 -0300

    🔨 Input text in nodes

--- src/automation-engine/components/Box/index.tsx
+++ src/automation-engine/components/Box/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { defaultBoxWidth, defaultBoxHeight } from '@/automation-engine/utils'
 import useDrag from '@/automation-engine/hooks/drag/useDrag'
 import { Node } from '@/automation-engine/models/node'
@@ -7,6 +7,13 @@ import styles from './box.module.scss'
 
 function Box({ node }: { node: Node }) {
   const ref = React.useRef<SVGRectElement>(null)
+  const [editMode, setEditMode] = useState(false)
+  const [inputValue, setInputValue] = useState('')
+  const handleDoubleClick = () => setEditMode(!editMode)
+  const handleInputToLabel = () => setEditMode(false)
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') handleInputToLabel()
+  }
   useDrag(ref, node.id)
 
   return (
@@ -20,7 +27,34 @@ function Box({ node }: { node: Node }) {
         height={defaultBoxHeight}
         fill="#058af0"
         className={styles.box}
+        onDoubleClick={handleDoubleClick}
       />
+      {editMode
+        ? (
+          <foreignObject x={node.x} y={node.y} width={defaultBoxWidth} height={defaultBoxHeight}>
+            <input
+              autoFocus
+              type="text"
+              value={inputValue}
+              onChange={(e) => setInputValue(e.target.value)}
+              onBlur={handleInputToLabel}
+              onKeyDown={handleKeyDown}
+              className={styles.blinkingCursor}
+            />
+          </foreignObject>
+        )
+        : (
+          <text
+            x={node.x + defaultBoxWidth / 2}
+            y={node.y + defaultBoxHeight / 2}
+            textAnchor="middle"
+            dominantBaseline="central"
+            style={{ pointerEvents: 'none' }}
+            className={styles.nonSelectable}
+          >
+            {inputValue}
+          </text>
+        )}
       <NewNode parentNode={node} />
     </>
   )

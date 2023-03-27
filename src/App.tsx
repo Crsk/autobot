111d6896772123464fd0a47ac7556ec0aba078dc
Christopher Kiessling <Crsk@mail.com>
2023-03-27 02:20:56 -0300

    📦 Implement `@adobe/spectrum` UI library

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,19 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import AutomationEngine from '@/automation-engine/components'
 import { Provider } from 'react-redux'
 import store from '@/redux/store'
+import { defaultTheme, Provider as SpectrumProvider } from '@adobe/react-spectrum'
 
 function App() {
   return (
     <div className="App">
       <Provider store={store}>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/flow" element={<AutomationEngine />} />
-          </Routes>
-        </BrowserRouter>
+        <SpectrumProvider theme={defaultTheme} colorScheme="light">
+          <BrowserRouter>
+            <Routes>
+              <Route path="/flow" element={<AutomationEngine />} />
+            </Routes>
+          </BrowserRouter>
+        </SpectrumProvider>
       </Provider>
     </div>
   )

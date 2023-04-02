0ac0697fb07fdb966399d433a050866b8a44d37f
Christopher Kiessling <Crsk@mail.com>
2023-04-02 04:53:25 -0400

    🔧 Show connection label closer to destination node

--- src/automation-engine/components/Connection/index.tsx
+++ src/automation-engine/components/Connection/index.tsx
@@ -3,7 +3,14 @@ import React from 'react'
 import { Point } from '@/automation-engine/types'
 import ConnectionTextLabel from '../ConnectionTextLabel'
 
-function getCurve(origin: Point, destination: Point) {
+// Helper function to calculate a point on a Bezier curve at a specific time (t)
+function bezierPoint(p0: number, p1: number, p2: number, p3: number, t: number) {
+  const mt = 1 - t
+
+  return mt ** 3 * p0 + 3 * mt ** 2 * t * p1 + 3 * mt * t ** 2 * p2 + t ** 3 * p3
+}
+
+function getCurve(origin: Point, destination: Point, targetPercentage: number = 0.8) {
   const { orientation, verticalDistance } = getNodesOrientation(origin, destination)
   const { originPoint: connectionOrigin, destinationPoint: connectionDest } = getConnectionPoints(origin, destination)
   const { x1, y1, x2, y2 } = { x1: connectionOrigin.x, y1: connectionOrigin.y, x2: connectionDest.x, y2: connectionDest.y }
@@ -21,8 +28,14 @@ function getCurve(origin: Point, destination: Point) {
   const cp2y = horizontal ? y2 : y2 - tensionY
   const pathD = `M ${x1} ${y1} C ${cp1x} ${cp1y}, ${cp2x} ${cp2y}, ${x2} ${y2}` // Create the Bezier curve
 
+  // Calculate a point along the curve that is a certain percentage of the total distance from the origin to the destination node
+  const pointDistance = distance * targetPercentage
+  const t = pointDistance / distance
+  const bx = bezierPoint(x1, cp1x, cp2x, x2, t)
+  const by = bezierPoint(y1, cp1y, cp2y, y2, t)
+
   return {
-    center: { x: (x1 + x2) / 2, y: (y1 + y2) / 2 },
+    point: { x: bx, y: by },
     curve: pathD,
     connection: {
       origin: connectionOrigin,
@@ -33,12 +46,13 @@ function getCurve(origin: Point, destination: Point) {
 }
 
 function Connection({ origin, destination }: { origin: Point, destination: Point }) {
-  const { center, curve, verticalDistance } = getCurve(origin, destination)
+  const { point, curve, verticalDistance } = getCurve(origin, destination)
 
   return (
     <>
       <path d={curve} stroke="darkcyan" strokeWidth={1.5} fill="none" />
-      <ConnectionTextLabel x={center.x} y={center.y} text="Yes" verticalDistance={verticalDistance} />
+      <circle cx={point.x} cy={point.y} r={3} fill="darkcyan" />
+      <ConnectionTextLabel x={point.x} y={point.y} text="Yes" verticalDistance={verticalDistance} />
     </>
   )
 }

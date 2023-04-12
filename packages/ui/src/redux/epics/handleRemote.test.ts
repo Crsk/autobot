901ba62a2e4805c2c86f02c8be06df99fdd17268
Christopher Kiessling <Crsk@mail.com>
2023-04-12 10:30:59 -0400

    test(ui): ensure `addNodeEpic` handles remote node creation

---
+++ packages/ui/src/redux/epics/handleRemote.test.ts
@@ -0,0 +1,27 @@
+import { describe, expect, it } from '@jest/globals'
+import { lastValueFrom, of } from 'rxjs'
+import { CreateNodePayload } from 'shared/src/types/dto'
+import { StateObservable } from 'redux-observable'
+import axios from 'axios'
+import MockAdapter from 'axios-mock-adapter'
+import nodeApi from '@/api/node/nodes.api'
+import { addNodeTrigger } from '@/redux/slices/nodeSlice'
+import { NodeActionTypes, RootState } from '@/redux/types'
+import addNodeEpic from './node/add.epic'
+
+describe('nodeEpic', () => {
+  it('should handle addNodeEpic remotly', async () => {
+    const mockAxios = new MockAdapter(axios)
+    const payload: CreateNodePayload = { id: 'test', name: 'test', x: 100, y: 100, parentId: '1' }
+    mockAxios.onPost(nodeApi.baseURL, payload).reply(200, { message: 'Success', success: true, payload })
+
+    const action$ = of(addNodeTrigger(payload))
+    const initialState: RootState = {} as RootState
+    const state$ = new StateObservable(of(initialState), initialState)
+    const dependencies = { nodeApi }
+    const output$ = addNodeEpic(action$, state$, dependencies)
+    const result = await lastValueFrom(output$)
+
+    expect(result).toEqual({ type: NodeActionTypes.ADD, payload })
+  })
+})

00eb21ab90cda73e5f3480714858f087c6868a10
Christopher Kiessling <Crsk@mail.com>
2023-04-12 12:30:57 -0400

    fix(*): update Jest configuration to allow test files anywhere in the monorepo

---
+++ jest.config.base.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+  roots: [
+    "<rootDir>/src"
+  ],
+  transform: {
+    "^.+\\.ts$": "ts-jest"
+  },
+  testRegex: "(/.*.(test|spec)).(tsx?|ts?)$",
+  moduleFileExtensions: [
+    "ts",
+    "tsx",
+    "js",
+    "jsx",
+    "json",
+    "node"
+  ],
+  collectCoverage: true,
+  coveragePathIgnorePatterns: [
+    "(tests/.*.mock).(tsx?|ts?)$"
+  ],
+  verbose: true
+}
--- packages/shared/jest.config.cjs
+++ packages/shared/jest.config.cjs
@@ -3,9 +3,4 @@ const base = require('../../jest.config.base.cjs')
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   ...base,
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1',
-  },
 }

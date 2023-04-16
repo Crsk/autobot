ce3146b185b36d5c96b7db78dc42efcc0f1408cb
Christopher Kiessling <Crsk@mail.com>
2023-04-16 11:20:07 -0400

    refactor(ui): hide sync button and connectivity bar

--- packages/ui/src/components/index.tsx
+++ packages/ui/src/components/index.tsx
@@ -4,21 +4,8 @@ import { useDispatch, useSelector } from 'react-redux'
 import Grid from '@/components/Grid'
 import Node from '@/components/Node'
 import Connection from '@/components/Connection'
-import { syncNodesTrigger } from '@/redux/slices/queueSlice'
 import { RootState } from '@/redux/types'
 import { fetchNodesTrigger } from '@/redux/slices/nodeSlice'
-import ConnectivityBar from './Connectivity/Connectivity'
-
-function Button() {
-  const dispatch = useDispatch()
-  const handleClick = () => navigator.onLine && dispatch(syncNodesTrigger())
-
-  return (
-    <foreignObject x="0" y="0" width="100" height="100">
-      <button type="button" onClick={handleClick}>Sync</button>
-    </foreignObject>
-  )
-}
 
 function AutomationEngine() {
   const dispatch = useDispatch()
@@ -34,25 +21,21 @@ function AutomationEngine() {
   }, [dispatch])
 
   return (
-    <>
-      <ConnectivityBar />
-      <svg width="100vw" height="100vh">
-        <Grid />
-        <Button />
-        {connections.map((connection) => (
-          connection.child && connection.parent && (
-            <Connection
-              key={`${connection.parent.id}-${connection.child.id}`}
-              origin={{ x: connection.parent.x, y: connection.parent.y }}
-              destination={{ x: connection.child.x, y: connection.child.y }}
-            />
-          )
-        ))}
-        {nodes && nodes.filter((x) => !!x).map((node: NodeType) => (
-          <Node key={node.id} node={node} />
-        ))}
-      </svg>
-    </>
+    <svg width="100vw" height="100vh">
+      <Grid />
+      {connections.map((connection) => (
+        connection.child && connection.parent && (
+          <Connection
+            key={`${connection.parent.id}-${connection.child.id}`}
+            origin={{ x: connection.parent.x, y: connection.parent.y }}
+            destination={{ x: connection.child.x, y: connection.child.y }}
+          />
+        )
+      ))}
+      {nodes && nodes.filter((x) => !!x).map((node: NodeType) => (
+        <Node key={node.id} node={node} />
+      ))}
+    </svg>
   )
 }
 

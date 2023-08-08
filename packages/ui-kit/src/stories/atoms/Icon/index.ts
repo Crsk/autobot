14fcc0c94a87fcd809a478b71bbe584b427f789d
Christopher Kiessling <Crsk@mail.com>
2023-08-07 20:25:48 -0400

    chore(ui-kit): add icons

---
+++ packages/ui-kit/src/stories/atoms/Icon/index.ts
@@ -0,0 +1,5 @@
+export { Open } from './Open.vector'
+export { Closed } from './Closed.vector'
+export { Public } from './Public.vector'
+export { View } from './View.vector'
+export { ViewClosed } from './ViewClosed.vector'
---
+++ packages/ui-kit/src/utils/colors.ts
@@ -0,0 +1,17 @@
+const colorsMap = {
+  gray25: {
+    dark: '#404040',
+    light: '#404040',
+  },
+  gray65: {
+    dark: '#a5a5a5',
+    light: '#a5a5a5',
+  },
+  // color when a nav item is set to private
+  private: {
+    dark: '#cda6ff',
+    light: '#ad71fc',
+  },
+}
+
+export { colorsMap }
